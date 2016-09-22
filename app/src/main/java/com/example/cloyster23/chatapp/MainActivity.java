8dca09a4d28522642464fefcecd8af73e5aedddc
RiasGremory <gogolol11@gmail.com>
2016-09-22 11:46:09 +0200

    åpner samtale

--- app/src/main/java/com/example/cloyster23/chatapp/MainActivity.java
+++ app/src/main/java/com/example/cloyster23/chatapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.cloyster23.chatapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -20,10 +23,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-  //      adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayOfKontakt);
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-   //     listView.setAdapter(adapter);
+        //      adapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,arrayOfKontakt);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //  setSupportActionBar(toolbar);
+        //     listView.setAdapter(adapter);
 
 
         ArrayList<Kontakt> arrayOfKontakt = new ArrayList<Kontakt>();
@@ -38,10 +41,16 @@ public class MainActivity extends Activity {
         kontaktAdapter kontAdapter = new kontaktAdapter(this, arrayOfKontakt);
         ListView listView = (ListView) findViewById(R.id.lvContacts);
         listView.setAdapter(kontAdapter);
-    }
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Kontakt kontakt = (Kontakt) parent.getAdapter().getItem(position);
+                Intent intent = new Intent(MainActivity.this, SamtaleActivity.class);
+                intent.putExtra("Kontakt", kontakt.getUsername());
+                startActivity(intent);
+            }
 
-    public void setSupportActionBar(Toolbar supportActionBar) {
-        this.supportActionBar = supportActionBar;
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/cloyster23/chatapp/SamtaleActivity.java
@@ -0,0 +1,16 @@
+package com.example.cloyster23.chatapp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Rias on 22.09.2016.
+ */
+
+public class SamtaleActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_samtale);
+    }
+}
